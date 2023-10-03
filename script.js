73abc499fbbf1a3c0b0b385a4db041f99bb9e28c
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-10-04 00:16:42 +0100

    Change of JS logic and update of HTML and CSS

--- script.js
+++ script.js
@@ -1,50 +1,94 @@
-// Definition of variables
-let userChoice;
+/*
+Logic to implement:
+1 - Add event for Space to change gameStarted to true
+2 - Once that happens, reset all variables
+3 - Once there is a click on the boxes (detected via event listener), run a playRound function
+4 - This function should take event id as playerSelection and run computerChoice()
+5 - Compute result of round and update all related fields (current score and table at the end)
+6 - Once a player reaches five, show some final match message and return gameStarted to false
+NOTE: Work so that the only global variable is gameStarted, all others can be defined inside functions
+*/
+
+
+// Global variable
+let playerSelection = '';
+let userScore, computerScore;
+let round = 0;
 const playOptions = ['rock', 'paper', 'scissors'];
 let gameStarted = false;
-let userScore = 0;
-let computerScore = 0;
+const buttons = document.getElementsByClassName("icon");
+const h1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
+const roundScore = document.getElementById("round-result");
+const currentScore = document.getElementById("score");
+const table = document.getElementById("table");
 
 // Start of the Game when Space key is pressed
 document.addEventListener("keyup", function(event) {
   if (event.key === " ") {
     if (!gameStarted) {
       gameStarted = true;
+      h2.textContent = "Select one of the three options below";
+      userScore = 0;
+      computerScore = 0;
+      roundScore.textContent = "";
+      currentScore.textContent = "";
       playGame();
     }
   }
 });
 
+// Start of game activates the buttons
+function playGame() {
+  Array.from(buttons).forEach( (button) => {
+    button.addEventListener("click", playRound );
+  });
+}
+
+function playRound(e) {
+  round++;
+  playerSelection = e.target.getAttribute("id");
+  computerChoice = getComputerChoice();
+  roundWinner(playerSelection, computerChoice);
+  updateScore(userScore, computerScore);
+  updateTable(playerSelection, computerChoice, userScore, computerScore);
+  checkEndOfMatch(userScore, computerScore);
+}
+
 // Function to generate Computer play
-let computerChoice = function getComputerChoice() {
+function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return playOptions[choice];
 }
 
-// Add event listener to buttons
-// To remember that getElementsByClassName doesn't generate an Array but an HTML Collection
-// This means that forEach method cannot be applied directly
-// Therefore I converted it to an Array using .from
-// It would also be possible to iterate through a for loop
-const buttons = document.getElementsByClassName("icon");
-const roundScore = document.getElementById("round-result");
-const currentScore = document.getElementById("score");
 
-Array.from(buttons).forEach( (button) => {
-  button.addEventListener("click", playerSelection);
-});
-
-function playerSelection() {
-  userChoice = this.getAttribute("id");
+// Function to check end of the match
+function checkEndOfMatch(user, computer) {
+  if (user === 5 || computer === 5) {
+    gameStarted = false;
+    Array.from(buttons).forEach( (button) => {
+      button.removeEventListener("click", playRound );
+    });
+    if (user === 5) {
+      roundScore.textContent = "Congratulations, you won!";
+    } else {
+      roundScore.textContent = "What a shame, you were beaten!";
+    }
+    h2.textContent = "Press Space to play again!";
+  }
 }
 
-function playGame() {
-  
+// Reset player and computer selection for next round
+function resetChoices() {
+  playerSelection = '';
+  computerChoice = '';
 }
 
+// Function to check the winner of each round and update the current result
 function roundWinner(player, computer) {
   if (player === computer) {
     roundScore.textContent = "It's a tie, let's keep playing!"
+    resetChoices()
     return;
   }
   
@@ -57,6 +101,7 @@ function roundWinner(player, computer) {
         roundScore.textContent = "He turned you into a Christmas gift!";
         computerScore++;
       };
+      resetChoices();
       break;
     case 'paper':
       if (computer === 'rock') {
@@ -66,6 +111,7 @@ function roundWinner(player, computer) {
         roundScore.textContent = "You were cut to pieces, what a shame!";
         computerScore++;
       };
+      resetChoices();
       break;
     case 'scissors':
       if (computer === 'paper') {
@@ -75,83 +121,12 @@ function roundWinner(player, computer) {
         roundScore.textContent = "That gotta have hurt!";
         computerScore++;
       };
+      resetChoices();
       break;
   }
 }
 
+// Function to update string literal with score after each round
 function updateScore(player, computer) {
   currentScore.textContent = `Player ${userScore} - Computer ${computerScore}`;
 }
-
-// ----------- TO BE USED ON NON-UI VERSION ----------- //
-// Function to retrieve play from user
-// Option for a do while loop to avoid inclusion of unexpected values
-// function getPlayerChoice() {
-//   let userChoice;
-//   do {
-//     let playerInput = prompt("Define your hand: ");
-//     userChoice = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
-//   } while (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors");
-//   return userChoice;
-// }
-
-// Function to determine the outcome of a round
-// function playRound(playerSelection, computerSelection) {
-//   if (playerSelection === "Rock") {
-//     if (computerSelection === "Rock") {
-//       return "It's a tie, you both selected Rock";
-//     } else if (computerSelection === "Paper") {
-//       return "You lose, Paper beats Rock";
-//     } else {
-//       return "You won, Rock beats Scissors";
-//     }
-//   } else if (playerSelection === "Paper") {
-//     if (computerSelection === "Rock") {
-//       return "You won, Paper beats Rock";
-//     } else if (computerSelection === "Paper") {
-//       return "It's a tie, you both selected Paper";
-//     } else {
-//       return "You lose, Scissors beats Paper";
-//     }
-//   } else {
-//     if (computerSelection === "Rock") {
-//       return "You lose, Rock beats Scissors";
-//     } else if (computerSelection === "Paper") {
-//       return "You won, Scissors beats Paper";
-//     } else {
-//       return "It's a tie, you both selected Scissors";
-//     }
-//   }
-// }
-
-// Function to play a 5 round game
-// Use of includes method for strings to control the result of each round
-function game() {
-  let player = 0;
-  let computer = 0;
-  for (let round = 0; round < 5; round++) {
-    let computerSelection = getComputerChoice();
-    let playerSelection = getPlayerChoice();
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    if (result.includes("You won")) {
-      player += 1;
-    } else if (result.includes("You lose")) {
-      computer += 1;
-    } else {
-      player += 1;
-      computer += 1;
-    }
-    console.log(`Score: Player ${player} - Computer ${computer}`);
-  }
-  if (player > computer) {
-    console.log("Congratulations, you won!");
-  } else if (player < computer) {
-    console.log("Shame on you, beaten by the CPU");
-  } else {
-    console.log(`01001001 01110100 00100111 01110011 00100000 01100001 00100000 01110100 01101001 01100101 00100001
-    Which means: It's a tie`);
-  }
-}
-
-// TODO: to avoid ties as end result, implement a do while loop that adds new rounds until any of the players wins!
